f23067ba24089bd3efd685b17060c5cc541d381c
dennistutuko <tutukodennis@gmail.com>
2020-07-27 21:22:11 +0700

    added h1 to page

--- monster-roladex/src/App.js
+++ monster-roladex/src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
 
     return(
       <div className="App">
+      <h1>Monsters Roladex</h1>
       <SearchBox 
         placeholder='search monsters'
         handleChange={e => this.setState({searchField : e.target.value})}
