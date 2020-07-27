0aa52634261af855102a12cf2f4ec2a438272577
dennistutuko <tutukodennis@gmail.com>
2020-07-27 22:53:14 +0700

    Updates

---
+++ precache-manifest.33748e2c6b4fb1b0e237bb782cbd599b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb0ec23d86b2a88d7c3716429e18214",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa2b9a61a16bf1ce703e",
+    "url": "/monsters-rolodex/static/css/main.2cb48326.chunk.css"
+  },
+  {
+    "revision": "773b5823b86ccd30b8ec",
+    "url": "/monsters-rolodex/static/js/2.643b3429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.643b3429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2b9a61a16bf1ce703e",
+    "url": "/monsters-rolodex/static/js/main.ec1f4fc0.chunk.js"
+  },
+  {
+    "revision": "f963425ca525eaa3548d",
+    "url": "/monsters-rolodex/static/js/runtime-main.c44364de.js"
+  }
+]);
